a54b40cab1227bd09caa2eb8ecbf13aaf46938a5
Lucas Rodrigues <lukinhasr1423@gmail.com>
2024-04-08 12:45:44 -0300

    adicionado novos controllers

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,11 +1,13 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SerieService {
@@ -13,12 +15,34 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repository.findAll()
-                .stream()
+        return converterDados(repository.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converterDados(repository.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO> converterDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(
                         s.getId(), s.getTitulo(), s.getTotalTemporadas(),
                         s.getAvaliacao(), s.getPoster(), s.getGenero(),
                         s.getAtores(), s.getSinopse()
                 )).toList();
+    }
+
+    public List<SerieDTO> obterLancamentos() {
+        return converterDados(repository.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
+                    s.getAvaliacao(), s.getPoster(), s.getGenero(),
+                    s.getAtores(), s.getSinopse());
+        }
+        return null;
     }
 }
